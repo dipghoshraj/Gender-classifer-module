bab16f42070937a4b8d2b3c197742dbe423b69d3
dipghosh <dip@spotkwik.com>
2021-04-28 15:45:39 +0530

    classifier model add

---
+++ gclassifier/classifier.py
@@ -0,0 +1,38 @@
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import numpy as np
+import cv2
+import os
+import cvlib as cv
+import requests
+
+
+def classify(frame):
+    lables = []
+    face, confidence = cv.detect_face(frame)
+
+
+    for idx, f in enumerate(face):
+        (startX, startY) = f[0], f[1]
+        (endX, endY) = f[2], f[3]
+
+        cv2.rectangle(frame, (startX,startY), (endX,endY), (0,255,0), 2)
+
+        face_crop = np.copy(frame[startY:endY,startX:endX])
+
+        if (face_crop.shape[0]) < 10 or (face_crop.shape[1]) < 10:
+            continue
+
+        face_crop = cv2.resize(face_crop, (96,96))
+        face_crop = face_crop.astype("float") / 255.0
+        face_crop = img_to_array(face_crop)
+        face_crop = np.expand_dims(face_crop, axis=0)
+
+        conf = model.predict(face_crop)[0] 
+        idx = np.argmax(conf)
+        label = classes[idx]
+        lables.append(label)
+
+    return lables
+
+
